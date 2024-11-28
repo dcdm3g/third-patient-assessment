de489a09e1f409bbb6125008a8f62deedf2c7388
Davi Melo <davicosta01082008@gmail.com>
2024-11-28 08:39:07 -0300

    feat: add dynamic patient and types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
 import Link from 'next/link'
 import { LatestAssessments } from '~/app/_components/latest-assessments'
 import { Button } from '~/components/ui/button'
+import { Input } from '~/components/ui/input'
+import { Label } from '~/components/ui/label'
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '~/components/ui/sheet'
+import { NewAssessmentForm } from './_components/new-assessment-form'
 
 export default function Home() {
   return (
@@ -8,9 +21,26 @@ export default function Home() {
       <div className="mx-4 flex w-full max-w-3xl flex-col gap-6">
         <div className="flex items-center justify-between">
           <h1 className="font-bold text-2xl">Latest Assessments</h1>
-          <Button variant="outline" asChild>
-            <Link href="/assessment">New Assessment</Link>
-          </Button>
+
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline">
+                New Assessment
+              </Button>
+            </SheetTrigger>
+
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle>Edit profile</SheetTitle>
+                <SheetDescription>
+                  Make changes to your profile here. Click save when you're
+                  done.
+                </SheetDescription>
+              </SheetHeader>
+
+              <NewAssessmentForm />
+            </SheetContent>
+          </Sheet>
         </div>
 
         <LatestAssessments />
